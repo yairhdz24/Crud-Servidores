a57ce5203038db8aad0d08c3a46f2ba32870c835
yairhdz24 <yairhdz107@gmail.com>
2024-05-02 18:59:28 -0600

    Actualizar archivos de conexión a la base de datos y consultas SQL

--- php/conexion.php
+++ php/conexion.php
@@ -7,8 +7,8 @@ $db_pass = "yairhdz24";
 $db_name = "Servidores";
 try {
     $con = new PDO('mysql:host=' . $db_host . ';port=' . $db_port . ';dbname=' . $db_name, $db_user, $db_pass);
-    echo 'conectado correctamente ' ;
+    //echo 'conectado correctamente ' ;
 } catch (Exception $e) {
-    echo 'No se pudo conectar a la base de datos : ' . $e->getMessage();
+    //echo 'No se pudo conectar a la base de datos : ' . $e->getMessage();
 }
 ?>
